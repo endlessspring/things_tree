dd6be56489696b5a5e3301cbcd6d26ddc811e63a
a.atabiev <endlessspring.of@gmail.com>
2022-09-15 23:45:28 +0300

    Taskpanel toolbar

--- src/components/TasksPanel/TasksPanel.header.tsx
+++ src/components/TasksPanel/TasksPanel.header.tsx
@@ -1,49 +1,68 @@
-import React, { useContext } from "react";
-import { observer } from "mobx-react-lite";
-import { TasksPanelContext } from "./TasksPanel";
-import { useForm } from "react-hook-form";
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
+import { TasksPanelContext } from './TasksPanel';
+import { useForm } from 'react-hook-form';
 
-import "./TasksPanel.header.scss";
-import icons from "../lib/icons";
-import { IconsEnum } from "../../types/icons.enum";
-import { Space } from "antd";
+import './TasksPanel.header.scss';
+import icons from '../lib/icons';
+import { IconsEnum } from '../../types/icons.enum';
+import { Button, Dropdown, Menu, Select, Space } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
 
 type Props = {};
 
 export const TasksPanelHeader: React.FC<Props> = observer(() => {
-  const { selected_list } = useContext(TasksPanelContext);
+    const { selected_list } = useContext(TasksPanelContext);
 
-  const { register, handleSubmit } = useForm({
-    mode: "all",
-    defaultValues: { name: "", description: "" },
-  });
+    const { register, handleSubmit } = useForm({
+        mode: 'all',
+        defaultValues: { name: '', description: '' },
+    });
 
-  const handleChange = handleSubmit(
-    (data) => {
-      selected_list?.setName(data.name);
-    },
-    (errors) => {
-      console.log(errors);
-    }
-  );
+    const handleChange = handleSubmit(
+        data => {
+            selected_list?.setName(data.name);
+        },
+        errors => {
+            console.log(errors);
+        }
+    );
 
-  return (
-    <div className={"tt-tasks_panel-header"}>
-      <form onChange={handleChange}>
-        <div style={{ fontSize: 20, display: "flex", alignItems: "center" }}>
-          <Space>
-            {icons[selected_list?.icon as IconsEnum]}
-            <input
-              className={"header-title-input"}
-              value={selected_list?.name}
-              {...register("name", {
-                minLength: 3,
-                disabled: selected_list?.is_system,
-              })}
-            />
-          </Space>
+    return (
+        <div className={'tt-tasks_panel-header'}>
+            <form onChange={handleChange}>
+                <div style={{ fontSize: 20, display: 'flex', alignItems: 'center' }}>
+                    <Space>
+                        {icons[selected_list?.icon as IconsEnum]}
+                        <input
+                            className={'header-title-input'}
+                            value={selected_list?.name}
+                            {...register('name', {
+                                minLength: 3,
+                                disabled: selected_list?.is_system,
+                            })}
+                        />
+
+                        <Dropdown
+                            overlay={
+                                <Menu
+                                    items={[
+                                        {
+                                            key: 1,
+                                            label: 'Удалить',
+                                            onClick: () => selected_list?.remove(),
+                                            disabled: selected_list?.is_system,
+                                        },
+                                    ]}
+                                />
+                            }
+                        >
+                            <Button icon={<EllipsisOutlined />} />  
+                        </Dropdown>
+                    </Space>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 });
+
