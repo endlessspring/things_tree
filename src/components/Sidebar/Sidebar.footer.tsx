c625503aa176d9187a5b25d0b3eac70c569a8b8a
a.atabiev <endlessspring.of@gmail.com>
2022-09-23 22:59:01 +0300

    Settings Button

--- src/components/Sidebar/Sidebar.footer.tsx
+++ src/components/Sidebar/Sidebar.footer.tsx
@@ -5,19 +5,22 @@ import { SidebarContext } from "./Sidebar";
 
 import "./sidebar.footer.scss";
 import { PlusOutlined } from "@ant-design/icons";
-import { Space } from "antd";
+import { Button, Space } from "antd";
+import icons from "../lib/Icons";
+import { IconsEnum } from "../../types/icons.enum";
 
 export const SidebarFooter: React.FC = observer(() => {
   const { onListAdd } = useContext(SidebarContext);
 
   return (
     <div className="tt-sidebar-footer">
-      <button className="footer-button" onClick={onListAdd}>
+      <button className="footer-add-button" onClick={onListAdd}>
         <Space align={"center"}>
           <PlusOutlined />
           Add List
         </Space>
       </button>
+      <button>{icons[IconsEnum.SETTINGS]}</button>
     </div>
   );
 });
