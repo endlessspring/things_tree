7b38b344e84a098dd97b55efe6ab24f85256d2df
a.atabiev <endlessspring.of@gmail.com>
2022-09-26 23:23:13 +0300

    Localization

--- src/i18n/ru.ts
+++ src/i18n/ru.ts
@@ -1,5 +1,12 @@
 export default {
     "common.settings": 'Настройки',
+    "common.today": 'Сегодня',
+    "common.inbox": 'Входящие',
+    "common.anytime": 'В любое время',
+    "common.sometime": 'Когда-нибудь',
+
+    "sidebar.addList": 'Добавить список',
+
     "lang.ru": 'Русский',
     "lang.en": 'Английский',
 }
