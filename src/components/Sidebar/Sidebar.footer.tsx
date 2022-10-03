fd3993759be0a08740667ef2068f821ca61b70f7
a.atabiev <endlessspring.of@gmail.com>
2022-10-03 23:12:48 +0300

    Localization

--- src/components/Sidebar/Sidebar.footer.tsx
+++ src/components/Sidebar/Sidebar.footer.tsx
@@ -1,26 +1,30 @@
-import { observer } from "mobx-react-lite";
+import {observer} from "mobx-react-lite";
 import React from "react";
-import { useContext } from "react";
-import { SidebarContext } from "./Sidebar";
+import {useContext} from "react";
+import {SidebarContext} from "./Sidebar";
+
+import {PlusOutlined} from "@ant-design/icons";
+import {Button, Space} from "antd";
+import icons from "../lib/Icons";
+import {IconsEnum} from "../../types/icons.enum";
 
 import "./sidebar.footer.scss";
-import { PlusOutlined } from "@ant-design/icons";
-import { Button, Space } from "antd";
-import icons from "../lib/Icons";
-import { IconsEnum } from "../../types/icons.enum";
+import {useTranslation} from "react-i18next";
 
 export const SidebarFooter: React.FC = observer(() => {
-  const { onListAdd, onSettingsOpen } = useContext(SidebarContext);
+    const {t} = useTranslation()
 
-  return (
-    <div className="tt-sidebar-footer">
-      <button className="footer-add-button" onClick={onListAdd}>
-        <Space align={"center"}>
-          <PlusOutlined />
-          Add List
-        </Space>
-      </button>
-      <button onClick={onSettingsOpen}>{icons[IconsEnum.SETTINGS]}</button>
-    </div>
-  );
+    const {onListAdd, onSettingsOpen} = useContext(SidebarContext);
+
+    return (
+        <div className="tt-sidebar-footer">
+            <button className="footer-add-button" onClick={onListAdd}>
+                <Space align={"center"}>
+                    <PlusOutlined/>
+                    {t('sidebar.addList')}
+                </Space>
+            </button>
+            <button onClick={onSettingsOpen}>{icons[IconsEnum.SETTINGS]}</button>
+        </div>
+    );
 });
