6fd8eb64af1e4690afe4144141b6de78abea8e68
a.atabiev <endlessspring.of@gmail.com>
2022-10-18 21:50:49 +0300

    Settings modal styles

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -6,6 +6,8 @@ import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
 import { THEMES_ENUM } from "../../types/themes.enum";
 
+import './style.scss';
+
 export default observer(() => {
   const { t } = useTranslation();
   const {
@@ -21,19 +23,29 @@ export default observer(() => {
       title={t("common.settings")}
       onCancel={handleCancel}
       footer={null}
+      wrapClassName={"settings-modal"}
     >
-      <Select value={lang} dropdownMatchSelectWidth onSelect={setLang}>
-        {Object.values(LANG_ENUM).map((lang) => (
-          <Select.Option value={lang}> {t(`lang.${lang}`)} </Select.Option>
-        ))}
-      </Select>
+      <div className="settings-modal-content">
+        <Select
+          className="settings-modal-select"
+          value={lang}
+          dropdownMatchSelectWidth
+          onSelect={setLang}>
+          {Object.values(LANG_ENUM).map((lang) => (
+            <Select.Option value={lang}> {t(`lang.${lang}`)} </Select.Option>
+          ))}
+        </Select>
 
-      <Select value={theme} dropdownMatchSelectWidth onSelect={setTheme}>
-        {Object.values(THEMES_ENUM).map((theme) => (
-          <Select.Option value={theme}> {t(`theme.${theme}`)} </Select.Option>
-        ))}
-      </Select>
-
+        <Select
+          className="settings-modal-select"
+          value={theme}
+          dropdownMatchSelectWidth
+          onSelect={setTheme}>
+          {Object.values(THEMES_ENUM).map((theme) => (
+            <Select.Option value={theme}> {t(`theme.${theme}`)} </Select.Option>
+          ))}
+        </Select>
+      </div>
     </Modal>
   );
 });
